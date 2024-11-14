2f4f3865601e8ce60467b4f2a16846dc1cc1d6ec
W0198745JC <w0198745@campus.nscc.ca>
2024-11-14 13:56:07 -0400

    pseudocode

--- A3-P2-MessageRedaction.py
+++ A3-P2-MessageRedaction.py
@@ -2,12 +2,16 @@
 #Description:   Design and write a program that counts and removes all desired letters from 
 #               any user-entered sentence or phrase.
 
-#Student #:     
-#Student Name:  
+#Student #: W0198745
+#Student Name:  Jenille Cheney 
 
 def main():
     # YOUR CODE STARTS HERE, each line must be indented (one tab)
-
+    #get input from user  Use a WHILE loop!
+    #make it a list 
+    #remove desired letters
+    #replace removed letters with underscore 
+    #print redacted input 
 
 
 
