9ec407dc678decb4a11f837dbebc6538fb77f273
W0198745JC <w0198745@campus.nscc.ca>
2024-11-15 10:20:01 -0400

    gettingthingsgoing

--- A3-P2-MessageRedaction.py
+++ A3-P2-MessageRedaction.py
@@ -7,12 +7,18 @@
 
 def main():
     # YOUR CODE STARTS HERE, each line must be indented (one tab)
+    phrase=""
+    redact=""
+    while (phrase != "quit"):
+        phrase= input("Type a phrase (or quit to exit program): ".lower())
+        redact= input("Type a comma - seperated list of letters to redact: ".lower())
+        redact.split (",")
     #get input from user  Use a WHILE loop!
     #make it a list 
     #remove desired letters
     #replace removed letters with underscore 
     #print redacted input 
-
+    print(f"Number of letters redacted: {}")
 
 
 
