de8f8922379a35708a35ba53cb91ee09f7ee5e45
W0198745JC <w0198745@campus.nscc.ca>
2024-11-18 12:19:01 -0400

    simplified to get it to work !!

--- A3-P2-MessageRedaction.py
+++ A3-P2-MessageRedaction.py
@@ -8,17 +8,31 @@
 def main():
     # YOUR CODE STARTS HERE, each line must be indented (one tab)
     phrase=""
-    redact=""
+    
+    redactLetters=[]
+
     while (phrase != "quit"):
         phrase= input("Type a phrase (or quit to exit program): ".lower())
         redact= input("Type a comma - seperated list of letters to redact: ".lower())
-        redact.split (",")
+        redactLetters = redact.split(",")
+    
+        print(redactLetters)
+        count=0
+        for letter in phrase:
+            if letter in redactLetters:
+                phrase=phrase.replace(letter, "_")
+                count+=1
+        print(phrase)
+
+
+        #.replace goes in a loop using the redactedLetters list BOOM!
+        
     #get input from user  Use a WHILE loop!
     #make it a list 
     #remove desired letters
     #replace removed letters with underscore 
     #print redacted input 
-    print(f"Number of letters redacted: {}")
+    #print(f"Number of letters redacted: {}")
 
 
 
